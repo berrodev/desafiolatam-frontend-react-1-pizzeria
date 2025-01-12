a4b4e7817e4ccffddefd67da3b9933275389d2bc
berrodev <fsberroeta @gmail.com>
2025-01-12 20:44:40 -0300

    feat: enhance App layout with navigation and improve CardPizza component styling

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,14 +1,25 @@
 const CardPizza = ({ name, price, ingredients, img }) => {
   return (
-    <div className="card">
-      <h3>{name}</h3>
-      <p>${price}</p>
-      <ul>
-        {ingredients.map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
-        ))}
-      </ul>
-      <img src={img} alt={name} />
+    <div className="max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden">
+      <img src={img} alt="Pizza Image" className="w-full h-48 object-cover" />
+      <div className="p-4">
+        <h2 className="text-xl font-bold text-gray-800 mb-2">{name}</h2>
+        <p className="text-gray-600 mb-4">
+          <span className="font-semibold">Ingredientes:</span>{' '}
+          {ingredients.join(', ')}
+        </p>
+        <p className="text-lg font-semibold text-gray-800 mb-4">
+          Precio: <span className="text-green-500">${price}</span>
+        </p>
+        <div className="flex justify-between">
+          <button className="px-4 py-2 text-sm text-white bg-blue-500 rounded hover:bg-blue-600">
+            Ver Más 👀
+          </button>
+          <button className="px-4 py-2 text-sm text-white bg-green-500 rounded hover:bg-green-600">
+            Añadir 🛒
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
