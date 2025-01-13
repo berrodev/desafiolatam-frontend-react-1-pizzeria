25cdf170dd7e7dc76c21634dd218fd726751e4c8
berrodev <fsberroeta @gmail.com>
2025-01-12 21:57:38 -0300

    feat: enhance CardPizza component layout for improved structure and responsiveness

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,17 +1,17 @@
 const CardPizza = ({ name, price, ingredients, img }) => {
   return (
-    <div className="max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden">
+    <div className="max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden flex flex-col ">
       <img src={img} alt="Pizza Image" className="w-full h-48 object-cover" />
-      <div className="p-4">
+      <div className="p-4 flex-1 flex flex-col">
         <h2 className="text-xl font-bold text-gray-800 mb-2">{name}</h2>
-        <p className="text-gray-600 mb-4">
+        <p className="text-gray-600 mb-4 flex-1">
           <span className="font-semibold">Ingredientes:</span>{' '}
           {ingredients.join(', ')}
         </p>
         <p className="text-lg font-semibold text-gray-800 mb-4">
           Precio: <span className="text-green-500">${price}</span>
         </p>
-        <div className="flex justify-between">
+        <div className="flex justify-between mt-auto">
           <button className="px-4 py-2 text-sm text-white bg-blue-500 rounded hover:bg-blue-600">
             Ver Más 👀
           </button>
