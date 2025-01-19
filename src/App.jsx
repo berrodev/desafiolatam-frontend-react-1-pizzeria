0a2573bfbd1ada6ba39fcd9808bb81ae1d7c21b7
berrodev <fsberroeta @gmail.com>
2025-01-19 14:41:22 -0300

    feat: refactor authentication components to use new page structure and separate form logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import Login from './components/Login';
-import Register from './components/Register';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   // const token = true;
