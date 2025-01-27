95b1cb2e491e6401eb3b079dc9cb9f698d692130
Francisco Berroeta <fsberroeta@gmail.com>
2025-01-26 23:54:53 -0300

    style: reorder class names in Footer component for consistency

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white text-center py-4">
+    <footer className="py-4 text-center text-white bg-gray-900">
       © 2025 - Pizza & Chill - All rights reserved.
     </footer>
   );
